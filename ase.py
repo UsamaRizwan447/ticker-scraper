61b03d707f623658aeb4c82207737f14033b8605
Muhammad Bilal Tahir <106396899+bilaltahir21@users.noreply.github.com>
2022-07-26 22:00:28 +0500

    Opening required report.

--- ase.py
+++ ase.py
@@ -18,8 +18,10 @@ driver.get(main_url)
 element = driver.find_elements(By.XPATH, "//ul[@class='tabs--primary nav nav-tabs']//a")[2]
 element.click()
 
-# element.clear()
-# element.send_keys("pycon")
-# element.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
+# Find the "Annual Financial Report" and open it
+elements = driver.find_elements(By.XPATH, "//tr[td/@headers='view-name-table-column' and td/@headers='view-filename-html-table-column']")
+for element in elements:
+    if "Annual Financial Report" in element.text:
+        element.find_element(By.XPATH, "//td[@headers='view-filename-html-table-column']").click()
+
 driver.close()
