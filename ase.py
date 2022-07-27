3b37de64d1645bb424f3102b4a4042ab16f24453
Muhammad Bilal Tahir <106396899+bilaltahir21@users.noreply.github.com>
2022-07-27 22:30:59 +0500

    Incoorporated list of tickers in code.

--- ase.py
+++ ase.py
@@ -1,18 +1,14 @@
-from lib2to3.pytree import type_repr
-import time
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Import webdriver and confirm that driver is compatible with Python
 driver = webdriver.Chrome()
-
-# TODO: replace this ticker with a for loop iterator
-ticker = "AALU"
-
-import pandas as pd
 
 # File name of xlsx file containing tickers
 file_name = 'sample_output.xlsx' 
@@ -24,75 +20,97 @@ tickers = []
 for tick in data.values:
     tickers.append(tick[0])
 
+for ticker in tickers:
+    # The main url for the company history page which will be used
+    main_url = f"https://www.ase.com.jo/en/company_historical/{ticker}"
 
-# The main url for the company history page which will be used
-main_url = f"https://www.ase.com.jo/en/company_historical/{ticker}"
+    driver.get(main_url)
 
-driver.get(main_url)
+    # Find the Disclosure tab from navbar and click
+    element = driver.find_elements(By.XPATH, "//ul[@class='tabs--primary nav nav-tabs']//a")[2]
+    element.click()
 
-# Find the Disclosure tab from navbar and click
-element = driver.find_elements(By.XPATH, "//ul[@class='tabs--primary nav nav-tabs']//a")[2]
-element.click()
+    # Find the "Annual Financial Report" and open it
+    elements = driver.find_elements(By.XPATH, "//tr[td/@headers='view-name-table-column' and td/@headers='view-filename-html-table-column']")
+    for element in elements:
+        if "Annual Financial Report" in element.text:
+            element.find_element(By.XPATH, "//td[@headers='view-filename-html-table-column']").click()
+            break
 
-# Find the "Annual Financial Report" and open it
-elements = driver.find_elements(By.XPATH, "//tr[td/@headers='view-name-table-column' and td/@headers='view-filename-html-table-column']")
-for element in elements:
-    if "Annual Financial Report" in element.text:
-        element.find_element(By.XPATH, "//td[@headers='view-filename-html-table-column']").click()
-        break
+    # Reports page URL
+    reports_url = driver.current_url
 
-# Reports page URL
-reports_url = driver.current_url
+    # Report that we need the data from
+    report_types = [ "Statement of financial position",
+                    "Income statement",
+                    "Statement of cash flows, indirect method",
+                    "Notes - Subclassifications of assets",
+                    "Notes - Subclassifications of liabilities and equities"
+                    ]
 
-# Report that we need the data from
-report_types = [ "Statement of financial position",
-                 "Income statement",
-                 "Statement of cash flows, indirect method",
-                 "Notes - Subclassifications of assets",
-                 "Notes - Subclassifications of liabilities and equities"
-                ]
+    reports_row_xpath = '//a[@ng-click="GetTaxonomyData(row)"]'
+    rendering_platform_body_xpath = "//div[@class='main-header']"
 
-elements = driver.find_elements(By.XPATH, '//a[@ng-click="GetTaxonomyData(row)"]')
-for element in elements:
-    if report_types[0].strip() == element.text.strip():
-        driver.implicitly_wait(3)
-        """Scrap data here"""
-        break
+    def page_load_by_element(element_xpath, timeout=30):
+        element_present = EC.presence_of_element_located((By.XPATH, element_xpath))
+        WebDriverWait(driver, timeout).until(element_present)
+        return True
 
-driver.get(reports_url)
-elements = driver.find_elements(By.XPATH, '//a[@ng-click="GetTaxonomyData(row)"]')
-for element in elements:
-    if report_types[1].strip() == element.text.strip():
-        driver.implicitly_wait(3)
-        """Scrap data here"""
-        break
+    WebDriverWait(driver, 5)
+    driver.find_element(By.XPATH, rendering_platform_body_xpath)
+    elements = driver.find_elements(By.XPATH, reports_row_xpath)
+    for element in elements:
+        if report_types[0].strip() == element.text.strip():
+            """Scrap data here"""
+            actions = ActionChains(driver).move_to_element(element)
+            actions.click(element).perform()
+            
+            break
 
-driver.get(reports_url)
-elements = driver.find_elements(By.XPATH, '//a[@ng-click="GetTaxonomyData(row)"]')
-for element in elements:
-    if report_types[2].strip() == element.text.strip():
-        driver.implicitly_wait(3)
-        """Scrap data here"""
-        break
+    driver.get(reports_url)
+    WebDriverWait(driver, 5)
+    driver.find_element(By.XPATH, rendering_platform_body_xpath)
+    elements = driver.find_elements(By.XPATH, reports_row_xpath)
+    for element in elements:
+        if report_types[1].strip() == element.text.strip():
+            """Scrap data here"""
+            actions = ActionChains(driver).move_to_element(element)
+            actions.click(element).perform()
+            break
 
-driver.get(reports_url)
-elements = driver.find_elements(By.XPATH, '//a[@ng-click="GetTaxonomyData(row)"]')
-for element in elements:
-    if report_types[3].strip() == element.text.strip():
-        driver.implicitly_wait(3)
-        """Scrap data here"""
-        break
+    driver.get(reports_url)
+    WebDriverWait(driver, 5)
+    driver.find_element(By.XPATH, rendering_platform_body_xpath)
+    elements = driver.find_elements(By.XPATH, reports_row_xpath)
+    for element in elements:
+        if report_types[2].strip() == element.text.strip():
+            """Scrap data here"""
+            actions = ActionChains(driver).move_to_element(element)
+            actions.click(element).perform()
+            break
 
-driver.get(reports_url)
-elements = driver.find_elements(By.XPATH, '//a[@ng-click="GetTaxonomyData(row)"]')
-for element in elements:
-    if report_types[4].strip() == element.text.strip():
-        driver.implicitly_wait(3)
-        print("Pressed 5")
-        """Scrap data here"""
-        break
-print("Exited 5")
+    driver.get(reports_url)
+    WebDriverWait(driver, 5)
+    driver.find_element(By.XPATH, rendering_platform_body_xpath)
+    elements = driver.find_elements(By.XPATH, reports_row_xpath)
+    for element in elements:
+        if report_types[3].strip() == element.text.strip():
+            """Scrap data here"""
+            actions = ActionChains(driver).move_to_element(element)
+            actions.click(element).perform()
+            break
 
-# This gets us all of the reports and we iterate through them and open the ones we need
+    driver.get(reports_url)
+    WebDriverWait(driver, 5)
+    driver.find_element(By.XPATH, rendering_platform_body_xpath)
+    elements = driver.find_elements(By.XPATH, reports_row_xpath)
+    for element in elements:
+        if report_types[4].strip() == element.text.strip():
+            """Scrap data here"""
+            actions = ActionChains(driver).move_to_element(element)
+            actions.click(element).perform()
+            break
+
+    # This gets us all of the reports and we iterate through them and open the ones we need
 
 driver.close()
